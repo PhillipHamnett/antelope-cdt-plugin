71333a3afa705c118a6703c635a68e61e657dca5
GitHub Action <action@github.com>
2024-09-17 08:16:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/philliphamnett/antelopecdtplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/philliphamnett/antelopecdtplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.philliphamnett.antelopecdtplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.philliphamnett.antelopecdtplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
